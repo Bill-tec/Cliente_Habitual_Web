f0d7dbbc93a944da0dd62c77b3e9b64dd5ff71cd
Raimundo <raimundohenryque@gmail.com>
2020-12-10 10:41:53 -0300

    Primeiro commit e push, aplicação Spring rodando com o index configurado inicialmente

---
+++ chweb/src/main/java/com/chweb/chweb/ChwebApplication.java
@@ -0,0 +1,13 @@
+package com.chweb.chweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChwebApplication.class, args);
+	}
+
+}
---
+++ chweb/src/test/java/com/chweb/chweb/ChwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chweb.chweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
